a592e57a94c3a90f21487c4d3c855cb572ffaf63
mfarinodinbeat <167328362+mfarinodinbeat@users.noreply.github.com>
2024-08-19 14:14:33 +0200

    category details add to the intreface

--- src/components/SplitCategories/Categories/index.js
+++ src/components/SplitCategories/Categories/index.js
@@ -8,22 +8,27 @@ import { toast, warn } from '@app/Omni';
 import { Languages } from '@common';
 import styles from './styles';
 import Item from './Item';
-
+// categories sidebar
 class Categories extends Component {
   render() {
     const { categories, selectedIndex, onPress } = this.props;
-
+    const mainCategories = this._getCategories(categories);
+    
     return (
       <View style={styles.container}>
         <ScrollView showsHorizontalScrollIndicator={false}>
-          {categories.map((item, index) => (
-            <Item
-              item={item}
-              key={index}
-              selected={index == selectedIndex}
-              onPress={() => onPress(index)}
-            />
-          ))}
+          {categories.map((item, index) => {
+            if(!this.isMainCategory(mainCategories, item.id))
+              return <></>;
+            return (
+              <Item
+                item={item}
+                key={index}
+                selected={index == selectedIndex}
+                onPress={() => onPress(index)}
+              />
+            )
+          })}
         </ScrollView>
       </View>
     );
@@ -34,6 +39,27 @@ class Categories extends Component {
       this.props.fetchCategories();
     }
   }
+
+  isMainCategory(cat, id){
+    for(let i=0;i<cat.length;i++) {
+      if(cat[i].id===id)
+        return true;
+    }
+    return false;
+  }
+
+  _getCategories = (categories, section) => {
+    if (categories && categories.length) {
+      return categories.filter(cate => {
+        if (section) {
+          return cate.parent === section.id; // check is sub category
+        }
+        return cate.parent === 0;
+      });
+    }
+    return [];
+  };
+
 }
 
 Categories.defaultProps = {
--- src/components/SplitCategories/index.js
+++ src/components/SplitCategories/index.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React, { Component } from 'react';
+import React, { Component, componentDidUpdate } from 'react';
 import {
   View,
   Image,
@@ -25,10 +25,44 @@ import {
 import styles from './styles';
 import Categories from './Categories';
 import ProductItem from '../HorizonLayout/ThreeColumn';
+import { ScrollView } from 'react-native';
+import Collapse from '../Collapse';
+import { WebView } from 'react-native-webview';
 
 class SplitCategories extends Component {
   state = {
     selectedIndex: 0,
+    history: "",
+    awards: "",
+    loading: false,
+    isAtTop: true,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.selectedIndex !== this.state.selectedIndex) {
+      this.setState({ loading:true });
+      this.fetchData();
+    }
+  }
+
+  fetchData = async () => {
+    try {
+      const response = await fetch('https://saborruralgalicia.com/wp-json/custom/v1/product-cat/'+this.props.categories[this.state.selectedIndex].id);
+      const data = await response.json();
+      const { history, awards } = data;
+      this.setState({ history, awards, loading:false });
+    } catch (error) {
+      console.error('Error al hacer la llamada a la API:', error);
+    }
+  };
+
+  handleScroll = (event) => {
+    const offsetY = event.nativeEvent.contentOffset.y;
+    if (offsetY <= 0) {
+      this.setState({ isAtTop: true });
+    } else {
+      this.setState({ isAtTop: false });
+    }
   };
 
   render() {
@@ -42,30 +76,275 @@ class SplitCategories extends Component {
     } = this.props;
 
     return (
-      <View style={[styles.container, { backgroundColor: background }]}>
+      <View style={[styles.container, { backgroundColor: background, height: '100%' }]}>
         <Categories
-          selectedIndex={this.state.selectedIndex}
+          selectedIndex={this.getCatFather(categories, categories[this.state.selectedIndex], this.state.selectedIndex)}//this.state.selectedIndex}
           onPress={this.selectCategory}
         />
         <View style={styles.content}>
-          {isFetching && (
-            <View style={styles.loading}>
-              <ActivityIndicator size="large" color={primary} />
+
+          <View>
+            {/* custom code subcategories */}
+            {
+              categories && categories.length > 0 &&
+              !this.isChild(categories[this.state.selectedIndex]) &&
+              categories[this.state.selectedIndex].image.src &&
+              <View
+                style={{
+                  padding: 10,
+                  marginBottom: 10,
+                  width: '100%'
+                }}
+              >
+                <Image
+                  source={{ uri: categories[this.state.selectedIndex].image.src }}
+                  style={{ width: '100%', height: 100, borderRadius: 5 }} 
+                  resizeMode="cover"
+                />
+              </View>
+            }
+            { categories && categories.length > 0 &&
+              !this.isChild(categories[this.state.selectedIndex]) ?
+              <FlatList
+                style={{
+                  padding: 10,
+                }}
+                data={this._getCategories(categories, categories[this.getCatFather(categories, categories[this.state.selectedIndex], this.state.selectedIndex)])}//categories[this.state.selectedIndex])}
+                renderItem={({item})=>(
+                  <TouchableOpacity
+                    key={item.id}
+                    style={{
+                      backgroundColor: this.isActive(categories, this.state.selectedIndex, item)?'rgba(0,0,0,.1)':'rgba(0,0,0,0)', 
+                      padding: 5, 
+                      borderRadius: 5,
+                      marginBottom: 10
+                    }}
+                    onPress={()=>{this.selectCategory(this.getCatIndexById(categories, item.id))}}
+                  >
+                    {
+                      item.image &&
+                      <View
+                        style={{marginBottom: 10}}
+                      >
+                        <Image
+                          source={{ uri: item.image.src }}
+                          style={{ width: 75, height: 75, borderRadius: 5 }} 
+                          resizeMode="cover"
+                        />
+                      </View>
+                    }
+                    <Text style={{ width: 75, textAlign: 'center' }}>{ item.name }</Text>
+                  </TouchableOpacity>
+                )}
+                numColumns={3}
+              />
+              : 
+              <ScrollView
+                horizontal
+                contentContainerStyle={{
+                  alignItems: 'center',
+                  paddingRight: 50,
+                  // height: 200,
+                  // maxHeight: 100
+                }}
+                style={{
+                  // height: 100,
+                  // maxHeight: 100,
+                  padding: 10,
+                  alignSelf: 'flex-start',
+                  width: '100%'
+                }}
+                showsHorizontalScrollIndicator={false}
+              >
+                {
+                categories && categories.length > 0 && 
+                categories.map((section, catIndex) => {
+                  const subCategories = this._getCategories(categories, section);
+                  if(section.id===categories[this.getCatFather(categories, categories[this.state.selectedIndex], this.state.selectedIndex)].id)
+                    return subCategories.map((item, index)=>(
+                      <TouchableOpacity
+                        key={item.id}
+                        style={{
+                          backgroundColor: this.isActive(categories, this.state.selectedIndex, item)?'rgba(0,0,0,.1)':'rgba(0,0,0,0)', 
+                          padding: 5, 
+                          borderRadius: 5,
+                          marginBottom: 10,
+                          alignItems: 'center', 
+                          alignSelf: 'flex-start',
+                        }}
+                        onPress={()=>{this.selectCategory(this.getCatIndexById(categories, item.id))}}
+                      >
+                        {
+                          item.image &&
+                          <View
+                            style={{marginBottom: 10}}
+                          >
+                            <Image
+                              source={{ uri: item.image.src }}
+                              style={{ width: 75, height: 75, borderRadius: 5 }} 
+                              resizeMode="cover"
+                            />
+                          </View>
+                        }
+                        <Text style={{ width: 75, textAlign: 'center' }}>{ item.name }</Text>
+                      </TouchableOpacity>
+                    ))
+                  return (<></>)
+                })
+                }
+              </ScrollView>
+            }
+            {/* custom code subcategories */}
+          </View>
+          {/* Child category */}
+          {
+            !isFetching && categories && categories.length > 0 &&
+            this.isChild(categories[this.state.selectedIndex]) &&
+            categories[this.state.selectedIndex].image.src &&
+            <View
+              style = {{
+                paddingHorizontal: 10
+              }}
+            >
+              <View
+                style={{
+                  // padding: 10,
+                  width: '100%',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: '#000',
+                    width: '100%', 
+                    height: 100,//this.state.isAtTop?150:75,
+                    borderRadius: 5,
+                    marginBottom: 5
+                  }}
+                >
+                  <Image
+                    source={{ uri: categories[this.state.selectedIndex].image.src }}
+                    style={{ width: '100%', height: 100, borderRadius: 5, opacity: .5 }} // this.state.isAtTop?150:75, borderRadius: 5, opacity: .5 }} 
+                    resizeMode="cover"
+                  />
+                </View>
+                <Text 
+                  style={{
+                    fontSize: 18, 
+                    position: 'absolute',
+                    color: '#fff'
+                  }}
+                >
+                  {categories[this.state.selectedIndex].name}
+                </Text>
+              </View>
+
+              <View
+                style={{
+                  marginBottom: 5,
+                  width: '100%'
+                }}
+              >
+                <Collapse
+                  text={this.state.history}
+                  title={"Historia"}
+                  loading={this.state.loading}
+                  loaderColor={primary}
+                />
+              </View>
+
+              <View
+                style={{
+                  marginBottom: 10,
+                  width: '100%'
+                }}
+              >
+                <Collapse
+                  text={""}
+                  title={"Premios"}
+                  loading={this.state.loading}
+                  loaderColor={primary}
+                >                
+                  <View style={{ width: '100%', height: 300 }}>
+                    <WebView
+                      originWhitelist={['*']}
+                      source={{ html: this.state.awards }} 
+                      style={{ height: 300 }}
+                      injectedJavaScript={`window.ReactNativeWebView.postMessage(document.body.scrollHeight);`}
+                    />
+                  </View>
+                </Collapse>
+              </View> 
             </View>
-          )}
-          {!isFetching && (
-            <FlatList
-              overScrollMode="never"
-              contentContainerStyle={styles.list}
-              data={products}
-              numColumns={2}
-              renderItem={this.renderItem}
-            />
-          )}
+          }
+          {/* Child category */}
+
+          {this.isChild(categories[this.state.selectedIndex]) &&
+          <>
+            {isFetching && (
+              <View style={styles.loading}>
+                <ActivityIndicator size="large" color={primary} />
+              </View>
+            )}
+            {!isFetching && (
+              <FlatList
+                overScrollMode="never"
+                contentContainerStyle={{ ...styles.list }}
+                data={products}
+                numColumns={2}
+                renderItem={this.renderItem}
+                onScroll={this.handleScroll}
+              />
+            )}
+          </>
+          }
         </View>
       </View>
     );
   }
+
+  isChild(section) {
+    if(!section)
+      return false;
+    if(section.parent)
+      return true;
+    return false;
+  }
+
+  getCatFather(cat, section, index) {
+    for(let i=0;i<cat.length;i++) {
+      if(cat[i].id===section.parent)
+          return i;
+    }
+    return index;
+  }
+
+  getCatIndexById(cat, id) {
+    for(let i=0;i<cat.length;i++){
+      if(cat[i].id===id)
+        return i;
+    }
+    return 0;
+  }
+
+  isActive(cat, act, sub) {
+    if(cat[act].id===sub.id)
+      return true;
+    return false; 
+  }
+
+  _getCategories = (categories, section) => {
+    if (categories && categories.length) {
+      return categories.filter(cate => {
+        if (section) {
+          return cate.parent === section.id; // check is sub category
+        }
+        return cate.parent === 0;
+      });
+    }
+    return [];
+  };
 
   renderItem = ({ item }) => {
     const { onViewPost } = this.props;
